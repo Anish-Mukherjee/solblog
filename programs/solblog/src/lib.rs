a59fc60a26d9a79466db64d7c1c1f47da50a9ae5
Anish <cybertronian217@gmail.com>
2022-10-16 13:08:48 +0530

    Added post update functionality ✅

--- programs/solblog/src/lib.rs
+++ programs/solblog/src/lib.rs
@@ -6,11 +6,7 @@ declare_id!("75UsBAF7211hxGY4Fzs2kMYAk3QTutTvxjYejA9TNfB4");
 pub mod solblog {
     use super::*;
 
-    pub fn initialize(
-        ctx: Context<Initialize>,
-        title: String,
-        content: String
-    ) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, title: String, content: String) -> Result<()> {
         let blog = &mut ctx.accounts.blog;
         blog.author = ctx.accounts.poster.key();
         blog.title = title;
@@ -18,6 +14,12 @@ pub mod solblog {
         blog.likes = 0;
         blog.dislikes = 0;
         msg!("Successfully posted blog");
+        Ok(())
+    }
+
+    pub fn update_post(ctx: Context<UpdatePost>, title: String, content: String) -> Result<()> {
+        let blog = &mut ctx.accounts.blog;
+        blog.content = content;
         Ok(())
     }
 }
@@ -32,11 +34,28 @@ pub struct Initialize<'info> {
     pub system_program: Program<'info, System>,
 }
 
+#[derive(Accounts)]
+#[instruction(title:String,content:String)]
+pub struct UpdatePost<'info> {
+    #[account(
+        mut,
+        seeds=[title.as_bytes(),poster.key().as_ref()],
+        bump,
+        realloc = 8+32+4+title.len()+4+content.len()+8+8,
+        realloc::payer = poster,
+        realloc::zero = true
+    )]
+    pub blog: Account<'info, Blog>,
+    #[account(mut)]
+    pub poster: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
 #[account]
 pub struct Blog {
-    pub author: Pubkey,
     pub title: String,
     pub content: String,
+    pub author: Pubkey,
     pub likes: u64,
     pub dislikes: u64,
 }
--- tests/solblog.ts
+++ tests/solblog.ts
@@ -1,12 +1,55 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
+import { expect } from "chai";
 import { Solblog } from "../target/types/solblog";
 
-describe("solblog", () => {
+describe("solblog", async () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env()
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.Solblog as Program<Solblog>;
 
-  
+  const blog = {
+    title: "The first blog",
+    content: "Lorem ipsum dolores amigos hasta la vista huevos rancheros"
+  }
+
+  const [blog_pda] = await anchor.web3.PublicKey.findProgramAddressSync([Buffer.from(blog.title), provider.wallet.publicKey.toBuffer()], program.programId)
+
+  it("Adds a new blog post", async () => {
+    await program.methods
+      .initialize(blog.title, blog.content)
+      .accounts({
+        blog: blog_pda
+      })
+      .rpc()
+
+    const account = await program.account.blog.fetch(blog_pda)
+
+    console.log(account.content)
+
+    expect(account.author === provider.wallet.publicKey)
+    expect(account.title === blog.title)
+    expect(account.content === blog.content)
+    expect(account.likes.toNumber() === 0)
+    expect(account.dislikes.toNumber() === 0)
+  })
+
+  it("Updates an existing post", async () => {
+    const new_content = "This is updated blog content!"
+
+    await program.methods
+      .updatePost(blog.title, new_content)
+      .accounts({
+        blog: blog_pda
+      })
+      .rpc()
+
+    const account = await program.account.blog.fetch(blog_pda)
+
+    console.log(account.content)
+
+    expect(account.content === new_content)
+  })
 });
